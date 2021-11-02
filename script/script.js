84c2bb5a5dc2863fd6ac19ef062bf5d2edca6073
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-11-01 21:52:00 -0400

    dropdown box done

--- script/script.js
+++ script/script.js
@@ -1,9 +1,29 @@
+const app = {
+    props: {
+        dropdownOnEvents: ['click', 'focusin', 'mouseenter'],
+        dropdownOffEvents: ['focusout', 'mouseleave']
+    },
+    utils: {
+        dropdownToggle: function (menu, box, dropdownEvents, condition) {
+            for (events of dropdownEvents) {
+                document.querySelector(menu).addEventListener(events, function(){
+                    document.querySelector(box).style.display = condition;
+                });
+            };
+        },
+    }
+}
 
-const dropdown = document.querySelectorAll('.dropdown-menu').forEach(eachMenu => {
-    eachMenu.addEventListener('click', function(){
-        // run this code when the <h1> is clicked
-        alert('You have successfully signed up for our newsletter!');
-    })
+//Header Dropdown box function
+app.utils.dropdownToggle('#navigation-menu', '#navigation-box', app.props.dropdownOnEvents, 'block');
+app.utils.dropdownToggle('#navigation-menu', '#navigation-box', app.props.dropdownOffEvents, 'none');
+app.utils.dropdownToggle('#shopping-menu', '#shopping-box', app.props.dropdownOnEvents, 'block');
+app.utils.dropdownToggle('#shopping-menu', '#shopping-box', app.props.dropdownOffEvents, 'none');
+app.utils.dropdownToggle('#searching-menu', '#searching-box', app.props.dropdownOnEvents, 'block');
+app.utils.dropdownToggle('#searching-menu', '#searching-box', app.props.dropdownOffEvents, 'none');
+
+//Navigation menu hover effect
+document.querySelector('#navigation-box li').addEventListener('mouseover', function() {
+    document.querySelector('#navigation-box li a').style.color = 'red';
+    console.log(this);
 });
-
-
