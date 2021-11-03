efba55ecd5f562f2573cdac958aec37a44045224
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-11-02 20:59:55 -0400

    gallery automatically displays function is done, and adjusted some styles for it

--- script/script.js
+++ script/script.js
@@ -5,16 +5,29 @@ const app = {
 
         imgs: ['#galleryPic1', '#galleryPic2', '#galleryPic3', '#galleryPic4','#galleryPic5', '#galleryPic6'],
         imgNo: 0,
-        zNo: 1
     },
     utils: {
         dropdownToggle: function (menu, box, dropdownEvents, condition) {
-            for (events of dropdownEvents) {
-                document.querySelector(menu).addEventListener(events, function(){
-                    document.querySelector(box).style.display = condition;
-                });
+            if (window.innerWidth < 1280) { //Nav dropdown box works until 1280px-width screen
+                for (events of dropdownEvents) {
+                    document.querySelector(menu).addEventListener(events, function(){
+                        document.querySelector(box).style.display = condition;
+                    });
+                };
             };
         },
+
+        autoPlayGallery: function (gallery) {
+            if (app.props.imgNo === gallery.length || app.props.imgNo === 0) {
+                app.props.imgNo = 0;
+                document.querySelector(gallery[app.props.imgNo + 5]).style.zIndex= "0"; // The 6th pic is hidden
+            } else {
+                document.querySelector(gallery[app.props.imgNo - 1]).style.zIndex= "0"; //Former pic is hidden
+            }
+            document.querySelector(gallery[app.props.imgNo]).style.zIndex= "1"; // Pic shows
+            document.querySelector(gallery[app.props.imgNo]).style.display = 'block';
+            app.props.imgNo ++ ;
+        }
     }
 }
 
@@ -28,32 +41,17 @@ app.utils.dropdownToggle('#searching-menu', '#searching-box', app.props.dropdown
 
 //Navigation menu hover effect
 document.querySelectorAll('#navigation-box li').forEach(function(effect) {
-    effect.addEventListener('mouseover', function() {
-        this.style.backgroundColor = '#999'; //$color-brown-grey
-        this.children[0].style.color = '#F6F6F6'; //$color-white
-    });
-    effect.addEventListener('mouseleave', function() {
-        this.style.backgroundColor = '#DDED9A'; //$color-green-light
-        this.children[0].style.color = '#2E2E2E'; //$color-black
-    });
+    if (window.innerWidth < 1280) { //Nav dropdown box works until 1280px-width screen
+        effect.addEventListener('mouseover', function() {
+            this.style.backgroundColor = '#999'; //$color-brown-grey
+            this.children[0].style.color = '#F6F6F6'; //$color-white
+        });
+        effect.addEventListener('mouseleave', function() {
+            this.style.backgroundColor = '#DDED9A'; //$color-green-light
+            this.children[0].style.color = '#2E2E2E'; //$color-black
+        });
+    };
 })
 
 //Gallery automatically displays
-function autoPlayGallery (gallery) {
-        app.props.imgNo ++;
-        console.log(app.props.imgNo);
-        if ( app.props.imgNo >= gallery.length) {
-            app.props.imgNo = 0;
-            console.log(`reset`);
-        }
-        document.querySelector(gallery[app.props.imgNo]).style.zIndex= app.props.zNo;
-        document.querySelector(gallery[app.props.imgNo]).style.display = 'block';
-        console.log(gallery[app.props.imgNo]);
-
-        app.props.zNo ++;
-        console.log(app.props.zNo);
-    
-}
-setInterval('autoPlayGallery(app.props.imgs)', 3000);
-
-
+setInterval('app.utils.autoPlayGallery(app.props.imgs)', 3000);
